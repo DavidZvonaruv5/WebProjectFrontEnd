92a887c7a380be5349f3b1bb5e58b0dcb3782cd3
DavidZvonaruv5 <102156879+DavidZvonaruv5@users.noreply.github.com>
2023-06-21 13:36:03 +0300

    Changes after deployment

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,18 +4,20 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1>Welcome to <span className="nowrap">TaskFlow</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>Located in Karmiel, TaskFlow lets it's employees see every task they have.</p>
+                <p>Every employee has an account created by its manager.</p>
+                <p>In your account you will find your tasks, you can see which ones youve completed, keep them or delete them. <br></br> You can assign tasks for other employees to!</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                   TaskFlow<br />
+                    Givat rimonit-ram 5<br />
+                    Karmiel, Israel<br />
+                    <a href="tel:+972543111631">Call us: 0543111631</a>
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>Owner: David & Gal</p>
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
