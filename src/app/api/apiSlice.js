3bc764245306e3d01e6552c9e24663d6b9c7de85
DavidZvonaruv5 <102156879+DavidZvonaruv5@users.noreply.github.com>
2023-06-21 12:46:36 +0300

    diployment ready

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -4,7 +4,7 @@ import {setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     //in development use localhost, in development we need to change that
-    baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://TaskFlow-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
